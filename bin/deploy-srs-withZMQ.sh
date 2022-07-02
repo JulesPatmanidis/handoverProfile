cbcdc78e9792c83a56f2fad6a5289b4417cbc8ce
JulesPatmanidis <ipat@hotmail.gr>
2022-07-02 19:16:53 +0300

    Test srsRan + ZMQ sh files

--- bin/deploy-srs-withZMQ.sh
+++ bin/deploy-srs-withZMQ.sh
@@ -1,43 +1,44 @@
 set -ex
-BINDIR=`dirname $0`
-source $BINDIR/common.sh
+#BINDIR=`dirname $0`
+# source $BINDIR/common.sh
+SRCDIR=/var/tmp
 
 cd $SRCDIR
 
 # Install dependencies
 sudo apt-get install cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev
 
-# Install ZMQ
-git clone https://github.com/zeromq/libzmq.git
-cd libzmq
-./autogen.sh
-./configure
-make
-sudo make install
-sudo ldconfig
-cd ..
-
-# Install CZMQ
-git clone https://github.com/zeromq/czmq.git
-cd czmq
-./autogen.sh
-./configure
-make
-sudo make install
-sudo ldconfig
-cd ..
-
-
-# Install srsRAN
-git clone https://github.com/srsRAN/srsRAN.git
-cd srsRAN
-mkdir build
-cd build
-cmake ../
-make
-sudo make install
-srsran_install_configs.sh user
-sudo ldconfig
+## Install ZMQ
+#git clone https://github.com/zeromq/libzmq.git
+#cd libzmq
+#./autogen.sh
+#./configure
+#make
+#sudo make install
+#sudo ldconfig
+#cd ..
+#
+## Install CZMQ
+#git clone https://github.com/zeromq/czmq.git
+#cd czmq
+#./autogen.sh
+#./configure
+#make
+#sudo make install
+#sudo ldconfig
+#cd ..
+#
+#
+## Install srsRAN
+#git clone https://github.com/srsRAN/srsRAN.git
+#cd srsRAN
+#mkdir build
+#cd build
+#cmake ../
+#make
+#sudo make install
+#srsran_install_configs.sh user
+#sudo ldconfig
 
 
 
